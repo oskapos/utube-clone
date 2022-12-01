9a4e71d6bb40dd0160270693f3cb1b857f264419
oskapos <omarblitz9@gmail.com>
2022-12-01 09:02:13 +0300

    fixed warnings

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -2,13 +2,7 @@ import { Typography, Card, CardContent, CardMedia } from '@mui/material';
 import { CheckCircle } from '@mui/icons-material';
 import { Link } from 'react-router-dom';
 
-import {
-  demoThumbnailUrl,
-  demoVideoUrl,
-  demoVideoTitle,
-  demoChannelUrl,
-  demoChannelTitle,
-} from '../utils/constants';
+import { demoVideoUrl, demoVideoTitle, demoChannelUrl, demoChannelTitle } from '../utils/constants';
 
 const VideoCard = ({
   video: {
